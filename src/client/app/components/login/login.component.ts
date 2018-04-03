2745fd227d5e90b58389207801d1e9451e49abcd
gopigoppu <gopi.learner93@gmail.com>
2018-04-03 23:53:42 +0530

    header and login page content add

--- src/client/app/components/login/login.component.ts
+++ src/client/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../shared/api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+
+  login(formFields) {
+    console.log(formFields);
+  }
 
   ngOnInit() {
   }
