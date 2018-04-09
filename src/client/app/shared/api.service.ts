239374f337ab45d2c2ef10fe3809100ea9c124a9
gopigoppu <gopi.learner93@gmail.com>
2018-04-09 08:33:09 +0530

    environment apiurl add

--- src/client/app/shared/api.service.ts
+++ src/client/app/shared/api.service.ts
@@ -1,15 +1,47 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable()
 export class ApiService {
 
-  readonly baseUrl = 'http://localhost:3000/api';
+  private baseUrl = environment.baseApiUrl;
 
   constructor(private http: HttpClient) { }
 
+
+
+  // request(url: string, method: string, body?: Object) {
+  //   const httpOptions = {
+  //     headers: new HttpHeaders({
+  //       'Content-Type': 'application/json'
+  //     })
+  //   };
+
+  //   const requestOptions = new RequestOptions({
+  //     url: `${this.baseUrl}${url}`,
+  //     method: method,
+  //     headers: httpOptions
+  //   });
+
+  //   if (body) {
+  //     requestOptions.body = body;
+  //   }
+
+  //   const request = new Request(requestOptions);
+
+  //   return this.http.request(request);
+  // }
+
+
   login(userData) {
-    return this.http.post(`${this.baseUrl}/login`, userData);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.post(`${this.baseUrl}login`, userData, httpOptions);
   }
 
 }
